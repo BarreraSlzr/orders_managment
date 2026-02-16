8bc32ee5a21cdb983759b6322971c2979ec3171b
Emmanuel Barrera Salazar (BarreraSlzr) <barreraslzr@gmail.com>
2026-02-16 01:29:59 -0600

    chore: migrate configuration from next.config.js to next.config.ts

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['images.ctfassets.net'],
-  },
-  //experimental: { serverActions: { allowedOrigins: [ "localhost:3000", "https://scaling-orbit-4jvqw474vj4cqxqp-3000.app.github.dev/", ], }, }
-}
-
-module.exports = nextConfig
